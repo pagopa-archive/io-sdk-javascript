fb2ade0913ca6df0fb764e93ac0e8f41f7425324
Michele Sciabarra <msciabarra@apache.org>
2020-02-24 09:25:17 +0100

    renamed to data

--- translate.test.js
+++ translate.test.js
@@ -2,7 +2,7 @@ const importer = require("./importer")
 const translate = require("./translate")
 
 test("translate", () => {
-    let imp = importer("data/test1.xlsx")
+    let imp = importer("data/data.xlsx")
     //console.log(imp)
     let tran = translate(imp[0])
     //console.log(tran)
